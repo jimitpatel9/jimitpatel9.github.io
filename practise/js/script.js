27c3b6c49aa3ef99acd473168a357f2ba942d800
jimitpatel9 <jdp59@njit.edu>
2015-08-24 14:12:19 -0400

    Update for Location services

--- practise/js/script.js
+++ practise/js/script.js
@@ -3,7 +3,7 @@
 
 	var app = angular.module("githubViewer",[]);
 	
-	var MainCtrl=function($scope,$http){
+	var MainCtrl=function($scope,$http,$interval,$log,$anchorScroll,$location){
 
 		var onUserComplete=function(response){
 			$scope.user = response.data;
@@ -13,6 +13,8 @@
 
 		var onRepos=function(response){
 			$scope.repos=response.data;
+			$location.hash("userDetails");
+			$anchorScroll();
 		};
 
 		var onError= function(reason){
@@ -20,13 +22,29 @@
 		};
 
 		$scope.search=function(username){
+			$log.info("Search for " + username);
 			$http.get("https://api.github.com/users/" + username)
 	 	.then(onUserComplete , onError);
+	 	if(countdownInterval){
+	 		$interval.cancel(countdownInterval);
+	 		$scope.countdown=null;
+	 	}
 	 };
-	 	
+	 	var decrementCountdown=function(){
+	 		$scope.countdown -=1;
+	 		if($scope.countdown<1){
+	 			$scope.search($scope.username);
+	 		}
+	 	};
+	 	var countdownInterval=null;
+	 	var startCountDown = function(){
+	 		countdownInterval = $interval(decrementCountdown,1000,$scope.countdown);
+	 	}
 		$scope.message="Github Viewer";
 		$scope.repoSortOrder="-stargzers_count";
 		$scope.username="angular";
+		$scope.countdown=5;
+		startCountDown();
 	};
 	app.controller("MainCtrl",MainCtrl);
 	}());
