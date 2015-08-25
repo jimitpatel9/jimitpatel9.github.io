6f490822711876c1e33fe8d5cafb951abb8d8bea
jimitpatel9 <jdp59@njit.edu>
2015-08-25 10:51:47 -0400

    Directives

---
+++ directives/js/script.js
@@ -0,0 +1,26 @@
+angular.module('app',[]);
+
+angular.module('app').controller('mainCtrl',function($scope) {
+	// body...
+	$scope.user = { 
+		name:"Jimit Patel",
+		address :{
+			street:"83 Jefferson Ave",
+			city:"Jersey City",
+			Planet: "Earth"
+		},
+		friends:[
+		'Rahul',
+		'Rinay',
+		'Ram'
+		]
+
+	};
+});
+angular.module('app').directive('userInfoCard',function() {
+	// body...
+	return{
+		templateUrl:"userInfoCard.html",
+		restrict:"E"
+	};
+});
