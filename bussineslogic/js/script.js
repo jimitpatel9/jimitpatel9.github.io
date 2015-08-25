6f6be03a453bae0156c329c31bf20838e4b11dcb
jimitpatel9 <jdp59@njit.edu>
2015-08-25 17:56:39 -0400

    business logic

---
+++ bussineslogic/js/script.js
@@ -0,0 +1,19 @@
+angular.module('app',[]);
+
+angular.module('app').controller('mainCtrl',function($scope) {
+	// body...
+	$scope.user1={
+		name:'Jimit'
+	}
+});
+angular.module('app').directive('userTile',function(){
+	return{
+		restrict:"E",
+		scope:{
+			user:"="
+		},
+		template:"userTile.html"
+		
+	}
+});
+
