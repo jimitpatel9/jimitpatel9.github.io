665d1955e66290d8603c2a6280938711791c8946
jimitpatel9 <jdp59@njit.edu>
2015-08-26 14:06:57 -0400

    scope understanding in transclude

--- businesstransclusion/js/script.js
+++ businesstransclusion/js/script.js
@@ -2,8 +2,17 @@ angular.module('app',[]);
 
 angular.module('app').controller('mainCtrl',function($scope) {
 	// body...
-	
+	$scope.baseLocation="jimit";
 });
-
+angular.module('app').directive('myQuestion',function(){
+	return{
+		restrict:'E',
+		transclude:true,
+		templateUrl:'myQuestion.html',
+		scope:{
+			questionText:'@q'
+		}
+	}
+});
 
 
