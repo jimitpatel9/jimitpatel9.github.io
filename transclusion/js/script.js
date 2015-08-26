b21a71486ebf8bc484538c72811734d7c084c5d7
jimitpatel9 <jdp59@njit.edu>
2015-08-26 13:03:53 -0400

    transclusion

---
+++ transclusion/js/script.js
@@ -0,0 +1,20 @@
+angular.module('app',[]);
+
+angular.module('app').controller('mainCtrl',function($scope) {
+	// body...
+	$scope.message="This Is message"
+});
+angular.module('app').directive('displayBox',function(){
+	return{
+		restrict:'E',
+		templateUrl:"displayBox.html",
+		controller:function($scope){
+			$scope.hidden =false;
+			$scope.clos=function(){
+				$scope.hidden=true;
+			}
+		},
+		transclude:true
+		}
+});
+
